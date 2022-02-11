ca99005f8d3d7ba353feec942ec102fcb6d188ff
Wenderson Júnio <wendersonjps@gmail.com>
2022-02-11 14:47:07 -0300

    Projeto finalizado

---
+++ frontend/src/components/admin/AdminPages.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="admin-page">
+        <PageTitle icon="fa fa-cogs" main="Administração do Sistema" sub="Cadastros"/>
+        <div class="admin-pages-tabs">
+            <b-card no-body>
+                <b-tabs card>
+                    <b-tab title="Artigos" active>
+                        <ArticleAdmin/>
+                    </b-tab>
+                    <b-tab title="Categorias" active>
+                        <CategoryAdmin/>
+                    </b-tab>
+                    <b-tab title="Usuários" active>
+                        <UserAdmin/>
+                    </b-tab>
+                </b-tabs>
+            </b-card>
+        </div>
+    </div>
+</template>
+
+<script>
+import PageTitle from "../template/PageTitle.vue"
+import ArticleAdmin from "./ArticleAdmin.vue"
+import CategoryAdmin from "./CategoryAdmin.vue"
+import UserAdmin from "./UserAdmin.vue"
+
+export default {
+    name: 'AdminPages',
+    components: {PageTitle, ArticleAdmin, CategoryAdmin, UserAdmin}
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/components/template/Header.vue
@@ -0,0 +1,79 @@
+<template>
+  <header class="header">
+      <a class="toggle" @click="toggleMenu" v-if="!hideToggle">
+          <i class="fa fa-lg" :class="icon"></i>
+      </a>
+      <h1 class="title">
+          <router-link to="/">{{title}}</router-link>
+      </h1>
+      <UserDropdown v-if="ihideUserDropdown"/>
+  </header>
+</template>
+
+<script>
+import UserDropdown from "./UserDropdown"
+export default {
+    name: 'Header',
+    components: {UserDropdown},
+    props: {
+        title: String,
+        hideToggle: Boolean,
+        hideUserDropdown: Boolean
+    },
+    computed: {
+        icon(){
+            return this.$store.state.isMenuVisible ? "fa-angle-left" : "fa-angle-down"
+        }
+    },
+    methods: {
+        toggleMenu(){
+            this.$store.commit('toggleMenu')
+        }
+    }
+}
+</script>
+
+<style>
+    .header {
+        grid-area: header;
+        background: linear-gradient(to right, #1e469a, #49a7c1);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .title {
+        font-size: 1.2rem;
+        color: white;
+        font-weight: 100;
+        flex-grow: 1;
+        text-align: center;
+    }
+
+    .title a {
+        color: white;
+        text-decoration: none;
+    }
+
+    .title a:hover {
+        color: white;
+        text-decoration: none;
+    }
+
+    header.header > a.toggle {
+        width: 60px;
+        height: 100%;
+        color: white;
+        justify-self: flex-start;
+        text-decoration: none;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    header.header > a.toggle:hover {
+        color: white;
+        background-color: rgba(0, 0, 0, 0.2);
+    }
+</style>
